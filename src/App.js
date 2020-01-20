a575a191ca17e3445ed7a7e1d2c88391f4cb63e6
Jian Zhang <jianzhang68@hotmail.com>
2020-01-20 11:01:46 +0100

    first comment

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello Jian Zhang!
         </a>
       </header>
     </div>
