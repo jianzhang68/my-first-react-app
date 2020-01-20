da9bb32b1bb87b4b349abdafc8f470259e8bb26d
Jian Zhang <jianzhang68@hotmail.com>
2020-01-20 11:27:30 +0100

    2de commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Jian Zhang!
+          Hello Jian Zhang and more!
         </a>
       </header>
     </div>
